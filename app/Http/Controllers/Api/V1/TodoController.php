8f694c3c5294ec54e32f62d3ebf1785d1e82042e
Mac <macelux@hotmail.com>
2022-05-14 08:06:05 +0100

    Add api adn todo controller

---
+++ app/Http/Controllers/Api/V1/TodoController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Services\TodoService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    public function __construct(protected TodoService $todoService)
+    {
+
+    }
+
+    public function index(): JsonResponse
+    {
+        $todos = $this->todoService->getTodos();
+
+        return $this->responseJson(true, 200, 'All todos fetched',$todos);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $validated_request = $request->validate([
+            'todo' => 'required'
+        ]);
+
+        $todos = $this->todoService->createTodo($validated_request);
+
+        return $this->responseJson(true, 200, $todos);
+    }
+
+    public function update(Request $request, $id): JsonResponse
+    {
+        $validated_request = $request->validate([
+            'todo' => 'required'
+        ]);
+
+        $todos = $this->todoService->updateTodo($validated_request, $id);
+
+        return $this->responseJson(true, 200, $todos);
+    }
+
+    public function destroy($id): JsonResponse
+    {
+        $todos = $this->todoService->deleteTodo($id);
+
+        return $this->responseJson(true, 200, $todos);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'todos'], function (){
+    Route::get('', [\App\Http\Controllers\Api\V1\TodoController::class, 'index']);
+    Route::post('', [\App\Http\Controllers\Api\V1\TodoController::class, 'store']);
+    Route::put('{id}', [\App\Http\Controllers\Api\V1\TodoController::class, 'update']);
+    Route::delete('{id}', [\App\Http\Controllers\Api\V1\TodoController::class, 'destroy']);
+});
